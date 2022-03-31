e99466cba682302cb0daf36a021a7aa163b48895
Lutfee <mynameislutfee@gmail.com>
2022-03-31 19:17:58 +0800

    revise GUI. improve progress bar. cleanup

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 import re
 import subprocess
 import time
+from glob import glob
 from subprocess import Popen, PIPE
 from pathlib import Path
 from tkinter import *
@@ -21,7 +22,7 @@ main_smali_file_path = ""
 
 window = Tk()
 window.title("Simple Obfuscator")
-window.geometry('480x300')
+window.geometry('520x500')
 window.config(background="light blue")
 
 
@@ -34,7 +35,10 @@ def open_file():
     global file_path
 
     file = askopenfile(mode='r')
-    progress["value"] += 20
+    #pogBar(progress)
+    #progress["value"] += 20
+
+
     window.update_idletasks()
     file_name = os.path.basename(file.name)
     file_path = file.name
@@ -42,12 +46,14 @@ def open_file():
 
     loaded = Label(window, text=f"{file_name} is loaded")
     loaded.config(anchor="s")
-    loaded.pack()
+    loaded.pack(side=TOP, pady=5)
+
 
     if file is not None:
         if file_name.__contains__(".apk"):
             print(file)
-            progress["value"] += 20
+            #progress["value"] += 10
+            pogBar(progress)
             window.update_idletasks()
             apk_decompile(file.name)
 
@@ -71,19 +77,26 @@ def apk_decompile(apk_file):
 
     loaded = Label(window, text=f"{file_name} successfully decompiled at output/{apk_name}")
     loaded.config(anchor=CENTER)
-    loaded.pack()
-    progress["value"] += 40
+    loaded.pack(side=TOP, pady=5)
+    progress["value"] += 10
     window.update_idletasks()
 
 
 # recompile and sign apk
 def apk_recompile_sign(apk_file):
+    pogBar(progress)
     apk_name = os.path.basename(apk_file)
     subprocess.call(f"java -jar tools/apktool.jar b -f -r --use-aapt2 output/{apk_file} -o data_files/obfuscated_apk/{apk_name}")
     time.sleep(3)
     subprocess.call(f"jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass password -keystore tools/test.keystore data_files/obfuscated_apk/{apk_name} test")
     subprocess.call(f"jarsigner -verify -verbose -certs data_files/obfuscated_apk/{apk_name}")
     subprocess.call(f"tools/zipalign -v 4 data_files/obfuscated_apk/{apk_name} data_files/obfuscated_apk/aligned-{apk_name}")
+    loaded = Label(window, text=f"{file_name} successfully recompiled at data_files/obfuscated_apk/aligned-{apk_name}")
+    loaded.config(anchor=CENTER)
+    loaded.pack(side=TOP, pady=5)
+    progress["value"] += 40
+
+
 
 
 
@@ -93,7 +106,7 @@ def obfuscate_smali_file():
     folder = os.listdir(f"output/{file_name}")
     print('folder', folder)
     count = 0
-
+    pogBar(progress)
     substring = "smali"
     substring2 = "example"
     for i in folder:
@@ -116,7 +129,8 @@ def obfuscate_smali_file():
                             addjunkcode(e)
     loaded = Label(window, text=f"{count} Smali file obfuscated!")
     loaded.config(anchor=CENTER)
-    loaded.pack()
+    loaded.pack(side=TOP, pady=5)
+    progress["value"] += 15
 
 def nocomment(inFile):
     print(inFile)
@@ -153,40 +167,40 @@ def addjunkcode(smaliCode):
     saltFUNCTIONreturn = "return-void\n"
     iterator = 0
 
-    #[ Function Salting ]
-    with open(smaliCode, "r") as fp:
-        lines = fp.readlines()
-        while iterator < len(lines):
-            if lines[iterator] == ".end method\n":
-                saltFUNCTIONfront = "\n.method public " + functionNameFirst[flag1] + functionNameSecond[flag2] + "\n"
-                if flag1 != 3 and flag2 != 3:
-                    lines.insert(iterator + 1, saltFUNCTIONfront)
-                    lines.insert(iterator + 2, saltFUNCTIONlocal)
-                    lines.insert(iterator + 3, saltFUNCTIONreturn)
-                    lines.insert(iterator + 4, saltFUNCTIONback)
-                    iterator += 5
-                    flag2 += 1
-                elif flag1 != 3 and flag2 == 3:
-                    flag1 += 1
-                    flag2 = 0
-                    lines.insert(iterator + 1, saltFUNCTIONfront)
-                    lines.insert(iterator + 2, saltFUNCTIONlocal)
-                    lines.insert(iterator + 3, saltFUNCTIONreturn)
-                    lines.insert(iterator + 4, saltFUNCTIONback)
-                    iterator += 5
-                elif flag1 == 3 and flag2 < 3:
-                    lines.insert(iterator + 1, saltFUNCTIONfront)
-                    lines.insert(iterator + 2, saltFUNCTIONlocal)
-                    lines.insert(iterator + 3, saltFUNCTIONreturn)
-                    lines.insert(iterator + 4, saltFUNCTIONback)
-                    iterator += 5
-                    flag2 += 1
-                elif flag1 == 3 and flag2 == 3:
-                    break
-            iterator += 1
-    f = open(smaliCode, "w")
-    f.writelines(lines)
-    f.close()
+    # #[ Function Salting ]
+    # with open(smaliCode, "r") as fp:
+    #     lines = fp.readlines()
+    #     while iterator < len(lines):
+    #         if lines[iterator] == ".end method\n":
+    #             saltFUNCTIONfront = "\n.method public " + functionNameFirst[flag1] + functionNameSecond[flag2] + "\n"
+    #             if flag1 != 3 and flag2 != 3:
+    #                 lines.insert(iterator + 1, saltFUNCTIONfront)
+    #                 lines.insert(iterator + 2, saltFUNCTIONlocal)
+    #                 lines.insert(iterator + 3, saltFUNCTIONreturn)
+    #                 lines.insert(iterator + 4, saltFUNCTIONback)
+    #                 iterator += 5
+    #                 flag2 += 1
+    #             elif flag1 != 3 and flag2 == 3:
+    #                 flag1 += 1
+    #                 flag2 = 0
+    #                 lines.insert(iterator + 1, saltFUNCTIONfront)
+    #                 lines.insert(iterator + 2, saltFUNCTIONlocal)
+    #                 lines.insert(iterator + 3, saltFUNCTIONreturn)
+    #                 lines.insert(iterator + 4, saltFUNCTIONback)
+    #                 iterator += 5
+    #             elif flag1 == 3 and flag2 < 3:
+    #                 lines.insert(iterator + 1, saltFUNCTIONfront)
+    #                 lines.insert(iterator + 2, saltFUNCTIONlocal)
+    #                 lines.insert(iterator + 3, saltFUNCTIONreturn)
+    #                 lines.insert(iterator + 4, saltFUNCTIONback)
+    #                 iterator += 5
+    #                 flag2 += 1
+    #             elif flag1 == 3 and flag2 == 3:
+    #                 break
+    #         iterator += 1
+    # f = open(smaliCode, "w")
+    # f.writelines(lines)
+    # f.close()
 
     # reset iterator [ Salt with NOP ]
     iterator = 0
@@ -236,12 +250,38 @@ def test():
 
     return None
 
+def clearFiles():
+    files = glob('data_files/obfuscated_apk/*')
+    for f in files:
+        os.remove(f)
+    loaded = Label(window, text="Folder Cleared")
+    loaded.config(anchor=CENTER)
+    loaded.pack(side=TOP, pady=5)
+    window.after(3000, destroy,loaded)
+
+def destroy(item):
+    item.destroy()
+
+
+
+
+def pogBar(bar):
+    for i in range(5):
+        bar["value"] += i
+        window.update_idletasks()
+        time.sleep(0.5)
+
+
 # -------------------- GUI Window --------------------
 
-btn = Button(window, text='Step 1: Open & Decompile', command=lambda: open_file()).pack(side=TOP, pady=10)
-#btn2 = Button(window, text='Remove Comments', command=lambda: nocomment()).pack(side=TOP, pady=10)
-btn3 = Button(window, text='Step 2: Obfuscate Smali', command=lambda: obfuscate_smali_file()).pack(side=TOP, pady=10)
-btn4 = Button(window, text='Step 3: Recompile', command=lambda: apk_recompile_sign(file_name)).pack(side=TOP, pady=10)
+btn1 = Button(window, text='Step 1: Open & Decompile APK', command=lambda: open_file()).pack(side=TOP, pady=10)
+btn2 = Button(window, text='Step 2: Obfuscate Smali', command=lambda: obfuscate_smali_file()).pack(side=TOP, pady=10)
+btn3 = Button(window, text='Step 3: Recompile', command=lambda: apk_recompile_sign(file_name)).pack(side=TOP, pady=10)
+loaded = Label(window, text="OPTIONAL")
+loaded.config(anchor=CENTER)
+loaded.pack(side=TOP, pady=10)
+btn4 = Button(window, text='Step 4: Compare Smali Files', command=lambda: test()).pack(side=TOP, pady=10)
+btn4 = Button(window, text='CLEAR RECOMPILED APK', command=lambda: clearFiles()).pack(side=TOP, pady=10)
 
 progress = Progressbar(window, orient=HORIZONTAL, length=400, mode="determinate")
 progress.pack(pady=20)
